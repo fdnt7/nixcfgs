d3ac4f04bc5eea0c8f23750ad0fa96fde66122b9
fdnt7 <43757589+fdnt7@users.noreply.github.com>
2025-09-16 22:15:00 +0700

    feat(cuties-only): fish shell git command abbrs
    <<< /run/current-system
    >>> /nix/store/xaqpyc84dcwg98zxk65b031val6cmx7f-nixos-system-cuties-only-25.11.20250913.c23193b
    
    SIZE: 7.17 GiB -> 7.17 GiB
    DIFF: 2.31 KiB

--- hosts/cuties-only/home-manager/programs/git.nix
+++ hosts/cuties-only/home-manager/programs/git.nix
@@ -1,17 +1,58 @@
 {nixcfgs, ...}: {
-  programs.git = let
-    userName = "${nixcfgs.uname}7";
-    userEmail = "43757589+${userName}@users.noreply.github.com";
-  in {
-    enable = true;
-    delta.enable = true;
-    signing = {
-      format = "ssh";
-      key = "~/.ssh/id_ed25519_github_${userName}_signing";
-      signByDefault = true;
+  programs = {
+    git = let
+      userName = "${nixcfgs.uname}7";
+      userEmail = "43757589+${userName}@users.noreply.github.com";
+    in {
+      enable = true;
+      delta.enable = true;
+      signing = {
+        format = "ssh";
+        key = "~/.ssh/id_ed25519_github_${userName}_signing";
+        signByDefault = true;
+      };
+      userEmail = userEmail;
+      userName = userName;
     };
-    userEmail = userEmail;
-    userName = userName;
+
+    fish.shellAbbrs = {
+      g = "git";
+      gg = "git clone";
+      gf = "git fetch";
+      gp = "git push";
+      gpf = "git push --force-with-lease";
+      gpu = "git push -u";
+      gu = "git pull";
+      gur = "git pull --rebase";
+      gl = "git log";
+      gd = "git diff";
+      ga = "git add";
+      "ga." = "git add .";
+      gm = "git merge";
+      gms = "git merge --squash";
+      gc = "git commit";
+      gca = "git commit --amend";
+      gs = "git status";
+      gb = "git branch";
+      gbd = "git branch -d";
+      gbm = "git branch -m";
+      gw = "git switch";
+      gwc = "git switch -c";
+      gh = "git show";
+      gt = "git stash";
+      gtl = "git stash list";
+      gtp = "git stash pop";
+      gv = "git revert";
+      ge = "git reset";
+      geh = "git reset --hard";
+      # no 'go' because go is a programming language
+      gov = "git remote -v";
+      goh = "git remote show";
+      gog = "git remote get-url";
+      gos = "git remote set-url";
+      goa = "git remote add";
+      gor = "git remote remove";
+    };
   };
 
   catppuccin.delta.enable = true;
