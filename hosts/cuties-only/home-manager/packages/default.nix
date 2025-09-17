5a1dbe21f79cda648f01e2561d2c52e4555fe753
fdnt7 <43757589+fdnt7@users.noreply.github.com>
2025-09-18 01:48:01 +0700

    feat(cuties-only): add devenv, brightnessctl mod
    <<< /run/current-system
    >>> /nix/store/vjspa8h5fy0xylrb5308qp68742259xm-nixos-system-cuties-only-25.11.20250913.c23193b
    
    CHANGED
    [U*] glibc-locales 2.40-66 -> 2.40-66 ×2
    
    ADDED
    [A.] boost         1.87.0
    [A.] cachix        1.7.9-bin
    [A.] devenv        1.8.2, 1.8.2-fish-completions
    [A.] devenv-nix    2.30.4
    [A*] nix           2.31pre20250712_b1245123
    [A.] nix-cmd       2.31pre20250712_b1245123
    [A.] nix-expr      2.31pre20250712_b1245123
    [A.] nix-fetchers  2.31pre20250712_b1245123
    [A.] nix-flake     2.31pre20250712_b1245123
    [A.] nix-main      2.31pre20250712_b1245123
    [A.] nix-store     2.31pre20250712_b1245123
    [A.] nix-util      2.31pre20250712_b1245123
    
    SIZE: 7.22 GiB -> 7.30 GiB
    DIFF: 79.0 MiB

--- hosts/cuties-only/home-manager/packages/default.nix
+++ hosts/cuties-only/home-manager/packages/default.nix
@@ -1,11 +1,12 @@
-{pkgs, ...}: {
+{
   imports = [
     ./brave.nix
+    ./brightnessctl.nix
+    ./devenv.nix
     ./nix-your-shell.nix
   ];
 
   # Add stuff for your user as you see fit:
   # programs.neovim.enable = true;
   # home.packages = with pkgs; [ steam ];
-  home.packages = with pkgs; [brightnessctl];
 }
