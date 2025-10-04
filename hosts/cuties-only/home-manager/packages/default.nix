6f5c940f3ac69213a71668feba66c99d5965784b
fdnt7 <43757589+fdnt7@users.noreply.github.com>
2025-10-04 18:45:52 +0700

    feat(cuties-only): add okular
    <<< /run/current-system
    >>> /nix/store/nlj99qmd4ilknx8x22pb0afx6d32qzdm-nixos-system-cuties-only-25.11.20251002.7df7ff7
    
    ADDED
    [A.] accounts-qt            1.17
    [A.] breeze-icons           6.18.0
    [A.] discount               3.0.1.2
    [A.] docbook-xml            4.5
    [A.] docbook-xsl-nons       1.79.2
    [A.] ebook-tools            0.2.2
    [A.] exiv2                  0.28.7-lib
    [A.] find-xml-catalogs-hook <none>
    [A.] ghostscript-with-X     10.05.1, 10.05.1-fonts
    [A.] ijs                    10.05.1
    [A.] jbig2dec               0.20
    [A.] kaccounts-integration  25.08.1
    [A.] karchive               6.18.0
    [A.] kauth                  6.18.0
    [A.] kbookmarks             6.18.0
    [A.] kcmutils               6.18.0
    [A.] kcodecs                6.18.0
    [A.] kcolorscheme           6.18.0
    [A.] kcompletion            6.18.0
    [A.] kconfig                6.18.0
    [A.] kconfigwidgets         6.18.0
    [A.] kcoreaddons            6.18.0
    [A.] kcrash                 6.18.0
    [A.] kdbusaddons            6.18.0
    [A.] kdeclarative           6.18.0
    [A.] kdegraphics-mobipocket 25.08.1
    [A.] kdoctools              6.18.0
    [A.] kglobalaccel           6.18.0
    [A.] kguiaddons             6.18.0
    [A.] ki18n                  6.18.0
    [A.] kiconthemes            6.18.0
    [A.] kio                    6.18.0
    [A.] kirigami               6.18.0
    [A.] kitemviews             6.18.0
    [A.] kjobwidgets            6.18.0
    [A.] knotifications         6.18.0
    [A.] kparts                 6.18.0
    [A.] kpty                   6.18.0
    [A.] kservice               6.18.0
    [A.] ktextwidgets           6.18.0
    [A.] kwallet                6.18.0
    [A.] kwidgetsaddons         6.18.0
    [A.] kwindowsystem          6.18.0
    [A.] kxmlgui                6.18.0
    [A.] libaccounts-glib       1.27, 1.27-dev
    [A.] libdmtx                0.7.8
    [A.] libkexiv2              25.08.1
    [A.] libspectre             0.2.12
    [A.] okular                 25.08.1, 25.08.1-fish-completions
    [A.] phonon                 4.12.0
    [A.] plasma-activities      6.4.5
    [A.] polkit-qt              1-0.200.0
    [A.] poppler-qt6            25.07.0
    [A.] prison                 6.18.0
    [A.] purpose                6.18.0
    [A.] qca                    2.3.10
    [A.] qqc2-desktop-style     6.18.0
    [A.] qtmultimedia           6.9.2
    [A.] qtquick3d              6.9.2
    [A.] qtspeech               6.9.2
    [A.] signond                8.61-unstable-2023-11-24
    [A.] solid                  6.18.0
    [A.] sonnet                 6.18.0
    [A.] threadweaver           6.18.0
    
    SIZE: 10.5 GiB -> 10.8 GiB
    DIFF: 311 MiB

--- hosts/cuties-only/home-manager/packages/default.nix
+++ hosts/cuties-only/home-manager/packages/default.nix
@@ -10,5 +10,10 @@
   # Add stuff for your user as you see fit:
   # programs.neovim.enable = true;
   # home.packages = with pkgs; [ steam ];
-  home.packages = with pkgs; [muse-sounds-manager musescore tenacity];
+  home.packages = with pkgs; [
+    kdePackages.okular
+    muse-sounds-manager
+    musescore
+    tenacity
+  ];
 }
