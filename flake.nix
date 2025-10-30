a4f9caec2798b4b2f94692309d2082328a477026
fdnt7 <43757589+fdnt7@users.noreply.github.com>
2025-10-30 22:10:32 +0700

    fix(flake): pin solaar to fork for chasing nixpkgs
    <<< /run/current-system
    >>> /nix/store/08hmbs7kmjsnp2i4j8488xs0q6af94bq-nixos-system-lavender-pilled-25.11.20251025.6a08e6b
    
    SIZE: 12.5 GiB -> 12.5 GiB
    DIFF: 144 bytes

--- flake.nix
+++ flake.nix
@@ -62,7 +62,8 @@
     solaar = {
       #url = "https://flakehub.com/f/Svenum/Solaar-Flake/*.tar.gz"; # For latest stable version
       #url = "https://flakehub.com/f/Svenum/Solaar-Flake/0.1.1.tar.gz"; # uncomment line for solaar version 1.1.13
-      url = "github:Svenum/Solaar-Flake/main"; # Uncomment line for latest unstable version
+      #url = "github:Svenum/Solaar-Flake/main"; # Uncomment line for latest unstable version
+      url = "github:fdnt7/Solaar-Flake/hotfix/wrap-g-apps-hook-3"; # https://github.com/Svenum/Solaar-Flake/issues/13
       inputs.nixpkgs.follows = "nixpkgs";
     };
 
