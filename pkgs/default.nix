ca6ad5d0acf907215beb851bbaf6af4b39f5d4f4
fdnt7 <43757589+fdnt7@users.noreply.github.com>
2025-12-13 19:43:34 +0700

    feat(pkgs/zed-discord-presence)!: pkg from nixpkgs
    <<< /run/current-system
    >>> /nix/store/pfqqs1bk8mqbrazb8b6bpj873fbk1vfm-nixos-system-lavender-pilled-26.05.20251211.2fbfb1d
    
    ADDED
    [A.] zed-discord-presence                                                           0.9.1, 0.9.1-fish-completions
    
    REMOVED
    [R.] discord-presence-lsp-e6f41ae3cbd2a7fcde1001a746422cdc673df8f3                  <none>
    [R.] discord-presence-lsp-e6f41ae3cbd2a7fcde1001a746422cdc673df8f3-fish-completions <none>
    
    SIZE: 13.4 GiB -> 13.4 GiB
    DIFF: -17.4 KiB

--- pkgs/default.nix
+++ pkgs/default.nix
@@ -2,6 +2,5 @@
 # You can build them using 'nix build .#example'
 pkgs: {
   # example = pkgs.callPackage ./example { };
-  zed-discord-presence = pkgs.callPackage ./zed-discord-presence.nix { };
   zed-wakatime = pkgs.callPackage ./zed-wakatime.nix { };
 }
