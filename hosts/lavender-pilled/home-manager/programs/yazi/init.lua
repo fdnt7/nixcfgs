fef2e6580e030effab0ec1a8d59b5b996c6f5947
fdnt7 <43757589+fdnt7@users.noreply.github.com>
2025-12-22 13:40:29 +0700

    feat(lavender-pilled): unround yazi indicator bar
    <<< /run/current-system
    >>> /nix/store/iggl6p1rg353hybjpxcvxwlcl9dgavnf-nixos-system-lavender-pilled-26.05.20251218.c6245e8
    
    SIZE: 13.4 GiB -> 13.4 GiB
    DIFF: 176 bytes

--- hosts/lavender-pilled/home-manager/programs/yazi/init.lua
+++ hosts/lavender-pilled/home-manager/programs/yazi/init.lua
@@ -1,3 +1,6 @@
+function Entity:padding() return " " end
+function Linemode:padding() return " " end
+
 require("full-border"):setup {
   -- Available values: ui.Border.PLAIN, ui.Border.ROUNDED
   type = ui.Border.PLAIN,
