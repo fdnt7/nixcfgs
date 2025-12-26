980abf6b779cf602576d75843d6c01b2225695f8
fdnt7 <43757589+fdnt7@users.noreply.github.com>
2025-12-27 00:29:14 +0700

    chore(pkgs/zed-wakatime): bump to latest upstream
    <<< /run/current-system
    >>> /nix/store/s2mnkvwc8i9d002dqhs1i5i7nhywdv0g-nixos-system-lavender-pilled-26.05.20251225.3e2499d
    
    ADDED
    [A.] wakatime-ls-d32a179962a28e714871bee09ba9f7a551163d3b <none>
    
    REMOVED
    [R.] wakatime-ls                                          379b2092cea447bcae910778ef8aadf48cc12940
    
    SIZE: 13.3 GiB -> 13.3 GiB
    DIFF: 152 bytes

--- pkgs/zed-wakatime.nix
+++ pkgs/zed-wakatime.nix
@@ -1,14 +1,14 @@
 { pkgs }:
 pkgs.rustPlatform.buildRustPackage rec {
   pname = "wakatime-ls";
-  version = "379b2092cea447bcae910778ef8aadf48cc12940";
+  version = "d32a179962a28e714871bee09ba9f7a551163d3b";
   cargoHash = "sha256-x2axmHinxYZ2VEddeCTqMJd8ok0KgAVdUhbWaOdRA30=";
 
   src = pkgs.fetchFromGitHub {
     owner = "wakatime";
     repo = "zed-wakatime";
     rev = version;
-    hash = "sha256-x0sZxf3maFT+jEufcJCTJBpGrsE+soibLvuFYkBwzXw=";
+    hash = "sha256-q9iWy4tt1M+0V4mXOwdhuNJMK1OpqpPQIicEyZJZj0g=";
   };
 
   cargoBuildFlags = "--package wakatime-ls";
