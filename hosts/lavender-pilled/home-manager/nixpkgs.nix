e0b9624bed61c503bf701c7607a3254b23e5a105
fdnt7 <43757589+fdnt7@users.noreply.github.com>
2026-01-04 20:01:25 +0700

    fix(flake,lavender-pilled): vesktop, unpin nixpkgs
    <<< /run/current-system
    >>> /nix/store/v73h8a6rlaflmq52jx0j338y0cicip91-nixos-system-lavender-pilled-26.05.20260102.fb7944c
    
    CHANGED
    [U*] bind                                          9.20.16-host, 9.20.16-lib, 9.20.16-man -> 9.20.17-host, 9.20.17-lib, 9.20.17-man
    [C.] busybox                                       1.36.1 ×2 -> 1.36.1, 1.37.0
    [U*] fish                                          4.3.1, 4.3.1-doc, 4.3.1-fish-completions, 4.3.1_fish-completions -> 4.3.2, 4.3.2-doc, 4.3.2_fish-completions, 4.3.2-fish-completions
    [U.] fishplugin-foreign-env                        0-unstable-2020-02-09 -> 0-unstable-2023-08-23
    [U.] gpsd                                          3.27.2 -> 3.27.3
    [U.] initrd-linux                                  6.18.2 -> 6.18.3
    [U.] libgphoto2                                    2.5.32 -> 2.5.33
    [U.] libwacom                                      2.16.1 -> 2.17.0
    [U.] linux                                         6.18.2, 6.18.2-modules ×2, 6.18.2-modules-shrunk -> 6.18.3, 6.18.3-modules ×2, 6.18.3-modules-shrunk
    [U.] mcpp                                          2.7.2.2 -> 2.7.2.3
    [U.] mesa                                          25.3.2 -> 25.3.3
    [U*] nix                                           2.31.2+1 ×2, 2.31.2+1-doc, 2.31.2+1-man -> 2.31.2+2 ×2, 2.31.2+2-doc, 2.31.2+2-man
    [U.] nix-cmd                                       2.30.3, 2.31.2+1 -> 2.30.3+1, 2.31.2+2
    [U.] nix-expr                                      2.30.3, 2.31.2+1 -> 2.30.3+1, 2.31.2+2
    [U.] nix-fetchers                                  2.30.3, 2.31.2+1 -> 2.30.3+1, 2.31.2+2
    [U.] nix-flake                                     2.30.3, 2.31.2+1 -> 2.30.3+1, 2.31.2+2
    [U.] nix-main                                      2.30.3, 2.31.2+1 -> 2.30.3+1, 2.31.2+2
    [U.] nix-manual                                    2.31.2+1, 2.31.2+1-man -> 2.31.2+2, 2.31.2+2-man
    [U.] nix-store                                     2.30.3, 2.31.2+1 -> 2.30.3+1, 2.31.2+2
    [U.] nix-util                                      2.30.3, 2.31.2+1 -> 2.30.3+1, 2.31.2+2
    [U.] nixos-system-lavender-pilled                  26.05.20251230.cad22e7 -> 26.05.20260102.fb7944c
    [U.] starship                                      1.24.1, 1.24.1-fish-completions -> 1.24.2, 1.24.2-fish-completions
    [U.] vesktop                                       1.6.1, 1.6.1-fish-completions -> 1.6.3, 1.6.3-fish-completions
    
    ADDED
    [A.] initrd-fstab                                  <none>
    [A.] initrd-system.conf                            <none>
    [A.] switch-inhibitors                             <none>
    [A.] unit-create-needed-for-boot-dirs.service      <none>
    [A.] unit-script-create-needed-for-boot-dirs-start <none>
    
    REMOVED
    [R.] unit-sysroot-run.mount                        <none>
    
    SIZE: 17.6 GiB -> 17.6 GiB
    DIFF: 2.76 MiB

--- hosts/lavender-pilled/home-manager/nixpkgs.nix
+++ hosts/lavender-pilled/home-manager/nixpkgs.nix
@@ -17,6 +17,30 @@
       #     patches = [ ./change-hello-to-hi.patch ];
       #   });
       # })
+      #
+
+      # temp fix for vesktop build failure
+      # ref: https://github.com/NixOS/nixpkgs/issues/476669#issuecomment-3707850520
+      (final: prev: {
+        vesktop = prev.vesktop.overrideAttrs (old: {
+          preBuild = ''
+            cp -r ${prev.electron.dist} electron-dist
+            chmod -R u+w electron-dist
+          '';
+          buildPhase = ''
+            runHook preBuild
+
+            pnpm build
+            pnpm exec electron-builder \
+              --dir \
+              -c.asarUnpack="**/*.node" \
+              -c.electronDist="electron-dist" \
+              -c.electronVersion=${prev.electron.version}
+
+            runHook postBuild
+          '';
+        });
+      })
     ];
     # Configure your nixpkgs instance
     config = {
