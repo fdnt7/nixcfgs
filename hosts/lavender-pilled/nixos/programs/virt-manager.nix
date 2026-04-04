f8c094e1b5bef3f5c7f4f9aae03ef401284635d0
fdnt7 <43757589+fdnt7@users.noreply.github.com>
2026-04-05 02:41:06 +0700

    feat(lavender-pilled)!: rm dnsmasq as is unneeded
    <<< /run/current-system
    >>> /nix/store/7ndkzzlsm49n0cwa0mq5hpmfwsxzs3rj-nixos-system-lavender-pilled-26.05.20260401.6201e20
    
    CHANGED
    [C-] dnsmasq 2.92_fish-completions, <others> -> <others>
    
    SIZE: 20.3 GiB -> 20.3 GiB
    DIFF: -22.9 KiB

--- hosts/lavender-pilled/nixos/programs/virt-manager.nix
+++ hosts/lavender-pilled/nixos/programs/virt-manager.nix
@@ -1,11 +1,6 @@
-{ pkgs, ... }:
 {
   virtualisation.libvirtd.enable = true;
-
   programs.virt-manager.enable = true;
-
-  environment.systemPackages = [ pkgs.dnsmasq ];
   networking.firewall.trustedInterfaces = [ "virbr0" ];
-
   persist.libvirt = true;
 }
