8a43590c2b86470d83e1b386a40c019527307e32
fdnt7 <43757589+fdnt7@users.noreply.github.com>
2026-04-05 02:24:49 +0700

    refactor(lavender-pilled): move vm firewall config
    <<< /run/current-system
    >>> /nix/store/kxbf77glqd4wr18rgxbmlylcl41yf1hh-nixos-system-lavender-pilled-26.05.20260401.6201e20
    
    SIZE: 20.3 GiB -> 20.3 GiB
    DIFF: 16 bytes

--- hosts/lavender-pilled/nixos/programs/virt-manager.nix
+++ hosts/lavender-pilled/nixos/programs/virt-manager.nix
@@ -1,7 +1,11 @@
 { pkgs, ... }:
 {
   virtualisation.libvirtd.enable = true;
+
   programs.virt-manager.enable = true;
+
   environment.systemPackages = [ pkgs.dnsmasq ];
+  networking.firewall.trustedInterfaces = [ "virbr0" ];
+
   persist.libvirt = true;
 }
