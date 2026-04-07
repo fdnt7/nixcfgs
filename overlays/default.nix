a8fd3eb5be3c48eb2928c73d270553c3bc0f33f7
fdnt7 <43757589+fdnt7@users.noreply.github.com>
2026-04-07 08:34:18 +0700

    revert: remove pinned nixpkgs for claude-code
    Refs: be1dec93ae1f3572fc73459841cf341fd1d4a778, 42cc7ce7067be0c21b54be17e99a47fc2127db71, 08c2672d05b43c969c32f3b659a5a9215caa9b32

--- overlays/default.nix
+++ overlays/default.nix
@@ -7,21 +7,11 @@
   # This one contains whatever you want to overlay
   # You can change versions, add patches, set compilation flags, anything really.
   # https://nixos.wiki/wiki/Overlays
-  modifications =
-    final: prev:
-    let
-      pinnedPkgs = import inputs.nixpkgs-14b7d17 {
-        system = final.stdenv.hostPlatform.system;
-        config = prev.config;
-      };
-    in
-    {
-      # example = prev.example.overrideAttrs (oldAttrs: rec {
-      # ...
-      # });
-
-      claude-code = pinnedPkgs.claude-code;
-    };
+  modifications = final: prev: {
+    # example = prev.example.overrideAttrs (oldAttrs: rec {
+    # ...
+    # });
+  };
 
   # When applied, the unstable nixpkgs set (declared in the flake inputs) will
   # be accessible through 'pkgs.unstable'
