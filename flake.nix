ac8040518d217f9464be78a2971794d998b77bd7
Fridella Nythell <43757589+fdnt7@users.noreply.github.com>
2026-04-19 00:17:06 +0700

    refactor(flake): use lib-defined systems list

--- flake.nix
+++ flake.nix
@@ -101,16 +101,17 @@
     }@inputs:
     let
       # Supported systems for your flake packages, shell, etc.
-      systems = [
-        "aarch64-linux"
-        "i686-linux"
-        "x86_64-linux"
-        "aarch64-darwin"
-        "x86_64-darwin"
-      ];
+      # systems = [
+      #   "aarch64-linux"
+      #   "i686-linux"
+      #   "x86_64-linux"
+      #   "aarch64-darwin"
+      #   "x86_64-darwin"
+      # ];
       # This is a function that generates an attribute by calling a function you
       # pass to it, with each system as an argument
-      forAllSystems = nixpkgs.lib.genAttrs systems;
+      # forAllSystems = nixpkgs.lib.genAttrs systems;
+      forAllSystems = nixpkgs.lib.genAttrs nixpkgs.lib.systems.flakeExposed;
 
       # Eval the treefmt modules from ./treefmt.nix
       treefmtEval = forAllSystems (
