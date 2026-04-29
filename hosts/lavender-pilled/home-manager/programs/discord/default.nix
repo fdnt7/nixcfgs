62a0072c6218d87685ec0560d95a11dd84e22b9f
Fridella Nythell <git@frdl.la>
2026-04-29 09:19:32 +0700

    fix(lavender-pilled): permit insecure discord dep
    Permit insecure package `openssl-1.1.1w` as Discord packages now depend
    on insecure OpenSSL 1.1.
    
    Refs: https://github.com/NixOS/nixpkgs/issues/513122
    
    <<< /run/current-system
    >>> /nix/store/373w8skcic2za4nj15j1hvaayk7xdk8z-nixos-system-lavender-pilled-26.05.20260422.0726a0e
    
    SIZE: 19.2 GiB -> 19.2 GiB
    DIFF: 184 bytes

--- hosts/lavender-pilled/home-manager/programs/discord/default.nix
+++ hosts/lavender-pilled/home-manager/programs/discord/default.nix
@@ -1,4 +1,10 @@
 {
+  # Discord packages now depend on insecure OpenSSL 1.1
+  # Refs: https://github.com/NixOS/nixpkgs/issues/513122
+  nixpkgs.config.permittedInsecurePackages = [
+    "openssl-1.1.1w"
+  ];
+
   imports = [
     ./canary.nix
     ./stable.nix
