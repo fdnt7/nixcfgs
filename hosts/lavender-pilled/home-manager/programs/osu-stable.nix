f2ca36337ebe5ec75732c921a029bcd3a7b87aad
Fridella Nythell <git@frdl.la>
2026-05-14 07:28:59 +0700

    feat(lavender-pilled)!: osu stable w/ xdg base dir
    Change the wine prefix location of `osu-stable` to
    `$XDG_DATA_HOME/osu-stable` instead of `$HOME/.osu` to comply with XDG
    base directory specification.
    
    <<< /run/current-system
    >>> /nix/store/zdyj7y1qc446lz64pshzvk71kvjni4bx-nixos-system-lavender-pilled-26.05.20260510.da5ad66
    
    SIZE: 21.1 GiB -> 21.1 GiB
    DIFF: 24 bytes

--- hosts/lavender-pilled/home-manager/programs/osu-stable.nix
+++ hosts/lavender-pilled/home-manager/programs/osu-stable.nix
@@ -1,4 +1,13 @@
-{ inputs, pkgs, ... }:
 {
-  home.packages = [ inputs.nix-gaming.packages.${pkgs.stdenv.hostPlatform.system}.osu-stable ];
+  config,
+  inputs,
+  pkgs,
+  ...
+}:
+{
+  home.packages = [
+    (inputs.nix-gaming.packages.${pkgs.stdenv.hostPlatform.system}.osu-stable.override {
+      location = "${config.xdg.dataHome}/osu-stable";
+    })
+  ];
 }
