f01650c854e278009584a942c816f8581ae50ecf
Fridella Nythell <git@frdl.la>
2026-05-16 22:24:00 +0700

    refactor(prefer-xdg-dirs): group app cfgs into var

--- modules/home-manager/prefer-xdg-directories.nix
+++ modules/home-manager/prefer-xdg-directories.nix
@@ -3,45 +3,80 @@
   lib,
   ...
 }:
-{
-  config =
-    let
-      inherit (config.xdg)
-        dataHome
-        stateHome
-        configHome
-        cacheHome
-        ;
-    in
-    lib.mkIf config.home.preferXdgDirectories {
-      nix.settings.use-xdg-base-directories = true;
+let
+  inherit (config.xdg)
+    dataHome
+    stateHome
+    configHome
+    cacheHome
+    ;
 
-      home.sessionVariables.GTK2_RC_FILES = "${configHome}/gtk-2.0/gtkrc";
+  android = {
+    home.sessionVariables.ANDROID_USER_HOME = "${dataHome}/android";
+  };
 
-      home.sessionVariables.ANDROID_USER_HOME = "${dataHome}/android";
+  bash = {
+    home.sessionVariables.HISTFILE = "${stateHome}/bash/history";
+  };
 
-      home.sessionVariables.HISTFILE = "${stateHome}/bash/history";
+  cargo = {
+    home.sessionVariables.CARGO_HOME = "${dataHome}/cargo";
+  };
 
-      home.sessionVariables.CARGO_HOME = "${dataHome}/cargo";
+  codex = {
+    home.sessionVariables.CODEX_HOME = "${configHome}/codex";
+  };
 
-      home.sessionVariables.CODEX_HOME = "${configHome}/codex";
+  elan = {
+    home.sessionVariables.ELAN_HOME = "${dataHome}/elan";
+  };
 
-      home.sessionVariables.ELAN_HOME = "${dataHome}/elan";
+  gtk2 = {
+    home.sessionVariables.GTK2_RC_FILES = "${configHome}/gtk-2.0/gtkrc";
+  };
 
-      home.sessionVariables.NODE_REPL_HISTORY = "${stateHome}/node_repl_history";
+  nix = {
+    nix.settings.use-xdg-base-directories = true;
+  };
 
-      home.sessionVariables.PYTHON_HISTORY = "${stateHome}/python_history";
-      home.sessionVariables.PYTHONPYCACHEPREFIX = "${cacheHome}/python";
-      home.sessionVariables.PYTHONUSERBASE = "${dataHome}/python";
+  node = {
+    home.sessionVariables.NODE_REPL_HISTORY = "${stateHome}/node_repl_history";
+  };
 
-      programs.fish = {
-        shellInit = ''
-          if type -q wget
-              alias wget="wget --hsts-file=${dataHome}/wget-hsts"
-          end
-        '';
-      };
-
-      home.sessionVariables.WAKATIME_HOME = "${configHome}/wakatime";
+  python = {
+    home.sessionVariables = {
+      PYTHON_HISTORY = "${stateHome}/python_history";
+      PYTHONPYCACHEPREFIX = "${cacheHome}/python";
+      PYTHONUSERBASE = "${dataHome}/python";
     };
+  };
+
+  wakatime = {
+    home.sessionVariables.WAKATIME_HOME = "${configHome}/wakatime";
+  };
+
+  wget = {
+    programs.fish.shellInit = ''
+      if type -q wget
+          alias wget="wget --hsts-file=${dataHome}/wget-hsts"
+      end
+    '';
+  };
+in
+{
+  config = lib.mkIf config.home.preferXdgDirectories (
+    lib.mkMerge [
+      android
+      bash
+      cargo
+      codex
+      elan
+      gtk2
+      nix
+      node
+      python
+      wakatime
+      wget
+    ]
+  );
 }
