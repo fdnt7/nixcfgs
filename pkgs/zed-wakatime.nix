1ff545707bff3e3f45fdda7e51dce0c2c402467a
Fridella Nythell <git@frdl.la>
2026-05-20 11:11:36 +0700

    chore(pkgs/zed-wakatime): bump to latest upstream

--- pkgs/zed-wakatime.nix
+++ pkgs/zed-wakatime.nix
@@ -1,14 +1,14 @@
 { pkgs }:
 pkgs.rustPlatform.buildRustPackage rec {
   pname = "wakatime-ls";
-  version = "8106d78fadc1dc0d255fd14662b031d56aac0f57";
-  cargoHash = "sha256-x2axmHinxYZ2VEddeCTqMJd8ok0KgAVdUhbWaOdRA30=";
+  version = "322b24666fb7731dbda0a1e62b69f8d19195e7c3";
+  cargoHash = "sha256-avOyVRYiI+BuE/c97vmlrWzD/Hbu9SzPdR7i0YTtOk4=";
 
   src = pkgs.fetchFromGitHub {
     owner = "wakatime";
     repo = "zed-wakatime";
     rev = version;
-    hash = "sha256-60TAw/3i0O/RldKkCYV4C6B/sSI0li+nxIV0aVyFL6s=";
+    hash = "sha256-fF+J9/dTTZriFjD24JoTiobjPSXMVFqe8fyl+ACB5HE=";
   };
 
   cargoBuildFlags = "--package wakatime-ls";
