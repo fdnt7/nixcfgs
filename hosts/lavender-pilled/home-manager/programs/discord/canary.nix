59f1036c7914a67051d838b01544d8396dbadea7
Fridella Nythell <git@frdl.la>
2026-05-23 18:13:12 +0700

    revert(lavender-pilled): enable dc canary openasar
    <<< /run/current-system
    >>> /nix/store/iv5xqz7n15jxpsn49zv22xhrk92nyxsg-nixos-system-lavender-pilled-26.05.20260521.f83fc3c
    
    SIZE: 24.1 GiB -> 24.1 GiB
    DIFF: -2.20 MiB
    
    Refs: a5c813eccf4c8cc010a490fad2e39a15ecd0ed85

--- hosts/lavender-pilled/home-manager/programs/discord/canary.nix
+++ hosts/lavender-pilled/home-manager/programs/discord/canary.nix
@@ -1,9 +1,4 @@
 { pkgs, ... }:
 {
-  # discord canary is stuck at installing update with openasar enabled
-  # Refs: https://github.com/NixOS/nixpkgs/issues/515106
-  home.packages = [
-    pkgs.discord-canary
-    # (pkgs.discord-canary.override { withOpenASAR = true; })
-  ];
+  home.packages = [ (pkgs.discord-canary.override { withOpenASAR = true; }) ];
 }
