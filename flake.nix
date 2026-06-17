e85b89d3f01ec88f3642c41770ae6e3f14a43cb1
Fridella Nythell <git@frdl.la>
2026-06-18 02:39:50 +0700

    revert(flake): unpin claude-desktop, works again
    Refs: 863b145, https://github.com/aaddrick/claude-desktop-debian/pull/723

--- flake.nix
+++ flake.nix
@@ -5,10 +5,7 @@
     # catppuccin.url = "github:catppuccin/nix";
 
     claude-desktop = {
-      # Pin to the latest working commit as the main branch is failing to build
-      #
-      # Refs: https://github.com/aaddrick/claude-desktop-debian/issues/718
-      url = "github:aaddrick/claude-desktop-debian/e85450c90ba38159f89f02bdd0f6c6d7e6bce065";
+      url = "github:aaddrick/claude-desktop-debian";
       inputs.nixpkgs.follows = "nixpkgs";
     };
 
