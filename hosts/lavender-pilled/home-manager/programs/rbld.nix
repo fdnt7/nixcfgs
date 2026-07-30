aa314d0cbaa772610b127635d05f79f637fd8c2e
Fridella Nythell <git@frdl.la>
2026-07-31 04:14:41 +0700

    feat(lavender-pilled): add shell abbrs for rbld
    Add fish shell abbreviations for `rbld`: `s` for `rbld switch` and `u`
    for `rbld update`.
    
    <<< /nix/store/kxy4pa348mzq3xrslrr13jcxvsv34j5j-nixos-system-lavender-pilled-26.11.20260729.0954f7e
    >>> /nix/store/ssfpwa24850wvj7vwyxvx60ccs4nkz4f-nixos-system-lavender-pilled-26.11.20260729.0954f7e
    
    PATHS: 2954 -> 2954 (+7, -7)
    SIZE: 25.0 GiB -> 25.0 GiB
    DIFF: +72 bytes

--- hosts/lavender-pilled/home-manager/programs/rbld.nix
+++ hosts/lavender-pilled/home-manager/programs/rbld.nix
@@ -1,8 +1,14 @@
 { inputs, nixcfgs, ... }: {
   imports = [ inputs.rbld.homeModules.default ];
 
-  programs.rbld = {
-    enable = true;
-    settings.flake = nixcfgs.flake;
+  programs = {
+    fish.shellAbbrs = {
+      s = "rbld switch";
+      u = "rbld update";
+    };
+    rbld = {
+      enable = true;
+      settings.flake = nixcfgs.flake;
+    };
   };
 }
