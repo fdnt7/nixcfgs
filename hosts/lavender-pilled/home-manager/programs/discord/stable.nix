6de48e6e56814e0504921734595b2d8dfdd910d6
Fridella Nythell <git@frdl.la>
2026-08-04 01:52:57 +0700

    feat(lavender-pilled): re-set vencord shiki theme
    Re-set the Vencord shiki codeblocks theme to the same Catppuccin Mocha
    theme but with the updated URL from a Nixcord update.
    
    Refs: 595d432, 25c66b5

--- hosts/lavender-pilled/home-manager/programs/discord/stable.nix
+++ hosts/lavender-pilled/home-manager/programs/discord/stable.nix
@@ -56,6 +56,7 @@
 
     shikiCodeblocks = {
       enable = true;
+      theme = "https://cdn.jsdelivr.net/gh/shikijs/textmate-grammars-themes@bc5436518111d87ea58eb56d97b3f9bec30e6b83/packages/tm-themes/themes/catppuccin-mocha.json";
     };
     serverInfo.enable = true;
     showHiddenThings.enable = true;
